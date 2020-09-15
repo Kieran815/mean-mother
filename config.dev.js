c0a733a3ef14cd0c7a8bbd08153f9cb5d1e61f7a
Kieran815 <kieran.milligan@gmail.com>
2020-09-15 10:41:33 -0500

    Configure session and cookie data

--- config.dev.js
+++ config.dev.js
@@ -1,3 +1,16 @@
 var config = {};
 config.mongodb = 'mongodb://localhost/mean-cms';
+
+// session config from mongodb
+config.session = {};
+
+// cookie config from mongo
+config.cookie = {};
+
+// random session secret
+config.session.secret = 'thx1138!lv271*'
+
+// cookie domain
+config.cookie.domain = 'localhost';
+
 module.exports = config;
