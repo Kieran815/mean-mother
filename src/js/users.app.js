32454dc8a228564a6d962088fc68695fd94e11e7
Kieran815 <kieran.milligan@gmail.com>
2020-09-16 15:06:44 -0500

    Display the users in an HTML table

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -1,23 +1,62 @@
 var usersApp = (function() {
-
+  // function to call api for data
   function viewUsers() {
+    // assign var to api location
     let uri = `${window.location.origin}/api/users`;
     let xhr = new XMLHttpRequest();
+    // set up `GET` request to api location
     xhr.open('GET', uri);
-
+    // add request headers
     xhr.setRequestHeader(
       "Content-Type",
       "application/json; charset=UTF-8"
     );
-
+    // SEND GET REQUEST
     xhr.send();
-
+    // on load (when request returns)
     xhr.onload = function() {
+      let app = document.getElementById('app');
       let data = JSON.parse(xhr.response);
-      console.log(data);
+      let users = data.users;
+      let table = '';
+      let rows = '';
+
+      for (let i = 0; i < users.length; i++) {
+        rows = rows + `<tr>
+          <td>
+            <a href="#view-${users[i]['_id']}">${users[i]["last_name"]}, ${users[i]["first_name"]}</a>
+          </td>
+          <td>${users[i]["username"]}</td>
+          <td>${users[i]["email"]}</td>
+        </tr>`;
+      }
+
+      table = `
+        <div class="card">
+          <div class="card-header clearfix">
+            <h2 class="h3 float-left">Users</h2>
+            <div class="float-right">
+              <a href="#create" class="btn btn-primary">New User</a>
+            </div>
+          </div>
+          <div class="table-responsive">
+            <table class="table table-striped table-hover table-bordered">
+              <thead>
+                <tr>
+                  <td>Name</td>
+                  <td>Username</td>
+                  <td>Email</td>
+                </tr>
+              </thead>
+              <tbody>${rows}</tbody>
+            </table>
+          </div>
+        </div>
+      `;
+      
+      app.innerHTML = table;
     }
   }
-
   return {
     load: function() {
       // alert('LOADED')
