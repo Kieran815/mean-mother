b2da275532b36f01a61058a4f4f6f9f209012283
Kieran815 <kieran.milligan@gmail.com>
2020-09-16 14:48:03 -0500

    Pull a list of users from the API

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/app', function(req, res, next) {
   // add a non-API endpoint
   res.render('users/app', {title: 'User Management'});
 });
